1d154a9bc138aa8473eae6a557ce40928f22d029
Virginia Dooley <vcdooley@gmail.com>
2018-09-10 16:46:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase()) 
+}
+
+function logWhisper(string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string) {
+  return string.toUpperCase()
+}
